4e3e47b9176bf1dd3d534dd0510dc77603c48e21
Alexander Burkut <a.burkut90@gmail.com>
2022-01-19 15:37:27 +0300

    Init project struct (#1)

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    rootDir: './',
+    automock: false,
+    preset: 'ts-jest',
+    clearMocks: true,
+    collectCoverage: true,
+    verbose: true,
+    silent: false,
+    coverageDirectory: 'coverage',
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+    ],
+    moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
+    testPathIgnorePatterns: [ '/node_modules/', '__snapshots__' ],
+    testMatch: [ '**/test/**/*.test.[jt]s?(x)' ],
+};
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import * as Bluebird from 'bluebird';
+
+// @ts-ignore
+global.Promise = Bluebird;
+
+process.on('unhandledRejection', (up) => { throw up; });
+process.on('exit', (code) => {
+  console.log(`Exit with code: ${code}`);
+});
+
+async function bootstrap() {
+  console.log('Bootstrap');
+}
+bootstrap();
